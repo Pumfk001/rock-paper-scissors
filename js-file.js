d0ff81d436a8fddd271023b8a5d87f409d6fd66b
Katie <katie.pumfrey@gmail.com>
2022-10-12 18:23:37 +0100

    Fix scissors button and add minor css

--- js-file.js
+++ js-file.js
@@ -21,53 +21,43 @@ const finalScore = document.querySelector('#finalResult');
 const rock = document.querySelector('#rock');
 rock.addEventListener('click', () => {
   playRound('rock',getComputerChoice()); 
-    result.textContent =`${roundResult}`; 
-    playScore.textContent ='Player: '+`${playerScore}`; 
-    compScore.textContent ='Computer: '+`${computerScore}`;
-    finalScore.textContent = null;
-    if(playerScore + computerScore == 5) {
-      if (playerScore > computerScore){
-        finalScore.textContent = 'Congrats, you win the game!'}
-      else {finalScore.textContent = 'Boo, you lost the game!'}
-      computerScore = 0; playerScore = 0;}
+  displayRound();
   });
-  const paper = document.querySelector('#paper');
+
+const paper = document.querySelector('#paper');
   paper.addEventListener('click', () => {
     playRound('paper',getComputerChoice()); 
-      result.textContent =`${roundResult}`; 
-      playScore.textContent ='Player: '+`${playerScore}`; 
-      compScore.textContent ='Computer: '+`${computerScore}`;
-      finalScore.textContent = null;
-      if(playerScore + computerScore == 5) {
-        if (playerScore > computerScore){
-          finalScore.textContent = 'Congrats, you win the game!'}
-        else {finalScore.textContent = 'Boo, you lost the game!'}
-        computerScore = 0; playerScore = 0;}
+    displayRound();
     });
+
 const scissors = document.querySelector('#scissors');
 scissors.addEventListener('click', () => {
-  playRound('rock',getComputerChoice()); 
-    result.textContent =`${roundResult}`; 
-    playScore.textContent ='Player: '+`${playerScore}`; 
-    compScore.textContent ='Computer: '+`${computerScore}`;
-    finalScore.textContent = null;
-    if(playerScore + computerScore == 5) {
-      if (playerScore > computerScore){
-        finalScore.textContent = 'Congrats, you win the game!'}
-      else {finalScore.textContent = 'Boo, you lost the game!'}
-      computerScore = 0; playerScore = 0;}
+  playRound('scissors',getComputerChoice()); 
+  displayRound();
   });
 
 
 //single game-round
 function playRound (playerChoice, computerChoice){
   // computerChoice = getComputerChoice ();
-  if (playerChoice.toLowerCase() === computerChoice) {return roundResult = "It's a draw, great minds think alike!"}
-    else if (playerChoice.toLowerCase() === 'rock' && computerChoice === 'paper') {++computerScore; return roundResult = "You lose the round! Paper beats rock!"}
-    else if (playerChoice.toLowerCase() === 'rock' && computerChoice === 'scissors') {++playerScore; return roundResult = "You win the round! Rock beats scissors!"}
-    else if (playerChoice.toLowerCase() === 'paper' && computerChoice === 'rock') {++playerScore; return roundResult = "You win the round! Paper beats rock!"}
-    else if (playerChoice.toLowerCase() === 'paper' && computerChoice === 'scissors') {++computerScore; return roundResult = "You lose the round! Scissors beat paper!"}
-    else if (playerChoice.toLowerCase() === 'scissors' && computerChoice === 'paper') {++playerScore; return roundResult = "You win the round! Scissors beat paper!"}
-    else if (playerChoice.toLowerCase() === 'scissors' && computerChoice === 'rock') {++computerScore; return roundResult = "You lose the round! Rock beats scissors!"}
-
+  if (playerChoice === computerChoice) {return roundResult = "It's a draw, great minds think alike!"}
+    else if (playerChoice === 'rock' && computerChoice === 'paper') {++computerScore; return roundResult = "You lose the round! Paper beats rock!"}
+    else if (playerChoice === 'rock' && computerChoice === 'scissors') {++playerScore; return roundResult = "You win the round! Rock beats scissors!"}
+    else if (playerChoice === 'paper' && computerChoice === 'rock') {++playerScore; return roundResult = "You win the round! Paper beats rock!"}
+    else if (playerChoice === 'paper' && computerChoice === 'scissors') {++computerScore; return roundResult = "You lose the round! Scissors beat paper!"}
+    else if (playerChoice === 'scissors' && computerChoice === 'paper') {++playerScore; return roundResult = "You win the round! Scissors beat paper!"}
+    else if (playerChoice === 'scissors' && computerChoice === 'rock') {++computerScore; return roundResult = "You lose the round! Rock beats scissors!"}
 }
+
+// display the score for each round and final result after 5 rounds are play
+function displayRound(){
+result.textContent =`${roundResult}`; 
+playScore.textContent ='Player: '+`${playerScore}`; 
+compScore.textContent ='Computer: '+`${computerScore}`;
+finalScore.textContent = null;
+if(playerScore == 3 || computerScore == 3) {
+  if (playerScore > computerScore){
+    finalScore.textContent = 'Congrats, you win the game!'}
+  else {finalScore.textContent = 'Boo, you lost the game!'}
+  computerScore = 0; playerScore = 0;}
+};
